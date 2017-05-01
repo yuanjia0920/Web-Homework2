3adea89a4ace702d02f1b025e3ef70fae10dc49a
yuanj <yuanj@DESKTOP-4UOV53P>
2017-05-01 11:14:32 -0700

    Add project files.

---
+++ Homework2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Homework2.Startup))]
+namespace Homework2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
